07b41ce27be3efeab691e45afc33c5bcaac5e232
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-07-16 19:54:20 +0300

    Task 3 : Revise some Layout designs

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Container>
-      <Grid container>
-        <Grid item xs={12} sm={12} md={4} lg={3} style={{backgroundColor : 'green'}}>
+    <Container className={"top_30"}>
+      <Grid container spacing={7}>
+        <Grid item xs={12} sm={12} md={4} lg={3}>
           <Profile/>
         </Grid>
         <Grid item xs={8} style={{backgroundColor : 'red'}}>
