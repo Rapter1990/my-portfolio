3df289fad8afb7da52c02022e866e82a080366cf
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-07-19 01:36:40 +0300

    Task 8 : Devise Portfolio layout page and Fix some layout typos

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,9 +1,96 @@
-import React from 'react'
+import { Card, CardActionArea, CardContent, CardMedia, Grid, Grow, Tab, Tabs, Typography } from '@material-ui/core';
+import React, { useState } from 'react'
+import PortfolioData from '../../util/PortfolioData';
+import portfolio_background from '../../assets/portfolio_background.jpg';
+import './Portfolio.css';
+
+import { Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core'
 
 const Portfolio = () => {
+
+  const [tabValue, setTabValue] = useState("All");
+  const [projectDialog, setProjectDialog] = useState(false);
+
   return (
     <div className='main_content'>
-      Portfolio
+      <Grid container spacing={1} className="section position-center">
+        {/** Title */}
+        <Grid item className="section_title top_20">
+          <span></span>
+          <h2>Portfolio</h2>
+        </Grid>
+
+        {/** Tab */}
+        <Grid item xs={12}>
+          <Tabs value={tabValue} 
+                indicatorColor="primary" 
+                className="customTabs"
+                onChange={(event, value) => setTabValue(value)}>
+
+                <Tab label="All" 
+                      value="All" 
+                      className={tabValue === "All" ? "customTabs_item active" : "customTabs_item" }  
+                  />
+                
+              {[...new Set(PortfolioData.portfolio.map(item => item.tag))].map(tag => (
+                <Tab label={tag} 
+                    value={tag}
+                    className={tabValue === tag ? "customTabs_item active" : "customTabs_item" } />
+              ))}
+
+          </Tabs>
+        </Grid>
+        
+        {/** Projects */}
+        <Grid item xs={12}>
+          <Grid item container spacing={4}>
+          {
+            PortfolioData.portfolio.map((project) => (
+              <>
+                {tabValue === "All" || project.tag === tabValue ? (
+                  <Grid key={tabValue} item xs={12} sm={6} md={4}>
+                    <Grow in timeout={1000}>
+                      <Card className='customCard' onClick={ () =>
+                        setProjectDialog(project)
+                      }>
+                        <CardActionArea>
+                          <CardMedia className='customCard_image' image={project.image ? project.image : portfolio_background} title={project.title}/>
+                          <CardContent/>
+                            <Typography className='customCard_title position-center'>{project.title}</Typography>
+                            <Typography variant='body2' className='customCard_caption'>{project.caption}</Typography>
+                        </CardActionArea>
+                      </Card>
+                    </Grow>
+                  </Grid>
+                ) : null}
+              </>
+            )
+          )}
+          </Grid>
+        </Grid>
+
+      </Grid>
+
+      <Dialog open={projectDialog !== false} onClose={() => setProjectDialog(false)} className="projectDialog" fullWidth>
+        <DialogTitle onClose={() => setProjectDialog(false)}> {projectDialog.title} </DialogTitle>
+        <img src={projectDialog.image} alt="" className='projectDialog_image'/>
+        <DialogContent className='projectDialog_description'>
+            {projectDialog.description}
+        </DialogContent>
+        <DialogActions className='projectDialog_actions'>
+
+          {projectDialog?.links && (
+      
+              projectDialog?.links 
+                ?.map((link, index) => (
+                    <a href={link.link} target="_blank" key={index} className="projectDialog_icons">{link.icon}</a>
+              ))
+            )
+          }
+
+        </DialogActions>
+      </Dialog>               
+
     </div>
   )
 }
---
+++ src/util/PortfolioData.js
@@ -0,0 +1,76 @@
+import {
+    GitHub,
+} from "@material-ui/icons";
+import portfolio_background from '../assets/portfolio_background.jpg';
+
+var PortfolioData = {
+    portfolio: [
+        {
+          tag: "React",
+          image: portfolio_background,
+          title: "Project 1",
+          caption: "A short description of the project",
+          description: "This is a project description. Please fill it in",
+          links: [
+            {
+              link: "",
+              icon: <GitHub />
+            }
+          ],
+        },
+        {
+          tag: "Java",
+          image: portfolio_background,
+          title: "Project 2",
+          caption: "A short description of the project",
+          description: "This is a project description. Please fill it in",
+          links: [
+            {
+              link: "",
+              icon: <GitHub />
+            }
+          ],
+        },
+        {
+          tag: "React",
+          image: portfolio_background,
+          title: "Project 3",
+          caption: "A short description of the project",
+          description: "This is a project description. Please fill it in",
+          links: [
+            {
+              link: "",
+              icon: <GitHub />
+            }
+          ],
+        },
+        {
+          tag: "Java",
+          image: portfolio_background,
+          title: "Project 4",
+          caption: "A short description of the project",
+          description: "This is a project description. Please fill it in",
+          links: [
+            {
+              link: "",
+              icon: <GitHub />
+            }
+          ],
+        },
+        {
+          tag: "Python",
+          image: portfolio_background,
+          title: "Project 5",
+          caption: "A short description of the project",
+          description: "This is a project description. Please fill it in",
+          links: [
+            {
+              link: "",
+              icon: <GitHub />
+            }
+          ],
+        },
+    ]
+}
+
+export default PortfolioData;
